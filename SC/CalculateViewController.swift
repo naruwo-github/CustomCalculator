8a8c071acc83c85c61afbed6b8729e4437920f0e
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-04-28 07:38:49 +0900

    チョコチョコレイアウト修正

--- SC/CalculateViewController.swift
+++ SC/CalculateViewController.swift
@@ -8,13 +8,127 @@
 
 import Foundation
 import UIKit
+import GoogleMobileAds
 
-class CalculateViewController: UIViewController {
+class CalculateViewController: UIViewController, GADBannerViewDelegate {
     @IBOutlet weak var topAdView: UIView!
     @IBOutlet weak var bottomAdView: UIView!
     @IBOutlet weak var calculatorView: UIView!
     
+    var topBannerView: GADBannerView!
+    var bottomBannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        topBannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        addTopBannerViewToView(topBannerView)
+        topBannerView.adUnitID = "ca-app-pub-6492692627915720/3353518937"
+        //Test
+        //topBannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        topBannerView.rootViewController = self
+        topBannerView.load(GADRequest())
+        topBannerView.delegate = self
+        
+        bottomBannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        addBottomBannerViewToView(bottomBannerView)
+        bottomBannerView.adUnitID = "ca-app-pub-6492692627915720/2126205352"
+        //Test
+        //bottomBannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bottomBannerView.rootViewController = self
+        bottomBannerView.load(GADRequest())
+        bottomBannerView.delegate = self
+        
+    }
+    
+    func addTopBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        if #available(iOS 11.0, *) {
+            positionBannerViewFullWidthAtTopOfSafeArea(bannerView)
+        }
+        else {
+            positionBannerViewFullWidthAtTopOfView(bannerView)
+        }
+    }
+    
+    func addBottomBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        if #available(iOS 11.0, *) {
+            positionBannerViewFullWidthAtBottomOfSafeArea(bannerView)
+        }
+        else {
+            positionBannerViewFullWidthAtBottomOfView(bannerView)
+        }
+    }
+    
+    // MARK: - view positioning
+    @available (iOS 11, *)
+    func positionBannerViewFullWidthAtTopOfSafeArea(_ bannerView: UIView) {
+        let guide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            guide.leftAnchor.constraint(equalTo: bannerView.leftAnchor),
+            guide.rightAnchor.constraint(equalTo: bannerView.rightAnchor),
+            guide.topAnchor.constraint(equalTo: bannerView.topAnchor)
+            ])
+    }
+    
+    @available (iOS 11, *)
+    func positionBannerViewFullWidthAtBottomOfSafeArea(_ bannerView: UIView) {
+        let guide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            guide.leftAnchor.constraint(equalTo: bannerView.leftAnchor),
+            guide.rightAnchor.constraint(equalTo: bannerView.rightAnchor),
+            guide.bottomAnchor.constraint(equalTo: bannerView.bottomAnchor)
+            ])
+    }
+    
+    func positionBannerViewFullWidthAtTopOfView(_ bannerView: UIView) {
+        view.addConstraint(NSLayoutConstraint(item: bannerView,
+                                              attribute: .leading,
+                                              relatedBy: .equal,
+                                              toItem: view,
+                                              attribute: .leading,
+                                              multiplier: 1,
+                                              constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: bannerView,
+                                              attribute: .trailing,
+                                              relatedBy: .equal,
+                                              toItem: view,
+                                              attribute: .trailing,
+                                              multiplier: 1,
+                                              constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: bannerView,
+                                              attribute: .top,
+                                              relatedBy: .equal,
+                                              toItem: topLayoutGuide,
+                                              attribute: .bottom,
+                                              multiplier: 1,
+                                              constant: 0))
+    }
+    
+    func positionBannerViewFullWidthAtBottomOfView(_ bannerView: UIView) {
+        view.addConstraint(NSLayoutConstraint(item: bannerView,
+                                              attribute: .leading,
+                                              relatedBy: .equal,
+                                              toItem: view,
+                                              attribute: .leading,
+                                              multiplier: 1,
+                                              constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: bannerView,
+                                              attribute: .trailing,
+                                              relatedBy: .equal,
+                                              toItem: view,
+                                              attribute: .trailing,
+                                              multiplier: 1,
+                                              constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: bannerView,
+                                              attribute: .bottom,
+                                              relatedBy: .equal,
+                                              toItem: bottomLayoutGuide,
+                                              attribute: .top,
+                                              multiplier: 1,
+                                              constant: 0))
     }
 }
